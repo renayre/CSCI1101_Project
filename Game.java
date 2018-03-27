69761abf50931763dcd59b0228db5e67be07ce8c
kate-yau <37847971+kate-yau@users.noreply.github.com>
2018-03-27 16:13:54 -0300

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,17 @@
+/*CSCI 1101 - MAFIA Project
+ * Kate, Elliot, Sasha, Allie, Rey */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Game {
+	Scanner kb = new Scanner(System.in);
+public Game(ArrayList<Character> l) {
+	
+}
+
+public Character vote() {
+	System.out.print("Time to vote on who to lynch. Press 1 for "+player1.getName()+", Press 2 for "+player2.getName()+", Press 3 for "+player3.getName()+", Press 4 for "+player4.getName()+", Press 5 for "+player5.getName()+", Press 6 for "+player6.getName()+", Press 7 for "+player7.getName()+" or Press 8 for "+player8.getName()+" :");
+		int choice = kb.nextInt(); //choice
+}
+}
