e832c4f6609185e3a9c85e2ab91c596f424128d4
kate-yau <37847971+kate-yau@users.noreply.github.com>
2018-03-27 16:50:11 -0300

    Update GameDemo.java

--- GameDemo.java
+++ GameDemo.java
@@ -9,20 +9,26 @@ public class GameDemo {
 		Scanner kb = new Scanner(System.in);
 		ArrayList<Integer> vote = new ArrayList<Integer>();
 		ArrayList<Character> players = new ArrayList<Character>(); 
+		ArrayList<Character> mafia = new ArrayList<Character>();
 		Random rn = new Random();
 		
 		System.out.print("Please enter your name: ");
-		String name = kb.nextLine();
+		String name = kb.next();
 		
-		int role = rn.nextInt(8);
+		int r = rn.nextInt(8);
+		Role role = assignRole(r);
 		
-		new Character(name,role);
+		players.add(name,role);
 		
-		
-		
-		
-
-
-		
+		for(int i=0; i<players.size(); i++) {
+			if(players.get(i).getIsMafia()==true) {
+				mafia.add(players.get(i));
+			}
+		}
 	}
+
+//mafiaNight
+public Character mafiaNight(){
+	
+}
 }
