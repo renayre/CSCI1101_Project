ff8b3d8c28ad1fd8be1756759d41910cb59ef573
sashap15 <37847974+sashap15@users.noreply.github.com>
2018-03-27 16:43:33 -0300

    Update Role.java

--- Role.java
+++ Role.java
@@ -1,4 +1,13 @@
 
-public class Role {
+/*CSCI 1101 - MAFIA MULTIPLAYER GAME - PROJECT
+ * Elliot, Renayre, Sasha, Allie and Kate */
+public abstract class Role {  //each role will have their own
+	private String role; 
+	
+	public Role() {}
+
+	public abstract String getRole();
+
+	public abstract String getRoleDescription(); //prints descriptions
 
 }
