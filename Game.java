723ec55847efd000946359e4a78ad635eb66d18b
sashap15 <37847974+sashap15@users.noreply.github.com>
2018-03-27 17:10:43 -0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,10 +7,14 @@ import java.util.ArrayList;
 public class Game {
 	Scanner kb = new Scanner(System.in);
 public Game(ArrayList<Character> l) {
+	l = new ArrayList<Character>();
 }
 public int vote() {
-	System.out.print("Time to vote on who to lynch. Press 1 for "+player1.getName()+", Press 2 for "+player2.getName()+", Press 3 for "+player3.getName()+", Press 4 for "+player4.getName()+", Press 5 for "+player5.getName()+", Press 6 for "+player6.getName()+", Press 7 for "+player7.getName()+" or Press 8 for "+player8.getName()+" :");
-		int choice = kb.nextInt(); //choice
-		return choice;
+	System.out.print("Time to vote on who to lynch.");
+	for(int i=0; i<l.size(); i++){
+		System.out.println("Press " + i + " for " + l.get(i).getName());
+	}
+	int choice = kb.nextInt(); //choice
+	return choice;
 }
 }
