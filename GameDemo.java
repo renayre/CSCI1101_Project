200416e223ae705e55f5e73b95ff5f516c9ef027
kate-yau <37847971+kate-yau@users.noreply.github.com>
2018-03-27 16:14:22 -0300

    Add files via upload

---
+++ GameDemo.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class GameDemo {
+	public static void main(String[]args) {
+		ArrayList<Integer> vote = new ArrayList<Integer>();
+		ArrayList<Character> players = new ArrayList<Character>(); 
+		
+		
+	}
+}
