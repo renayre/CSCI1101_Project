b76af903dab2a3114ec69216726addfc46c4c1b7
kate-yau <37847971+kate-yau@users.noreply.github.com>
2018-03-27 16:25:21 -0300

    Update GameDemo.java

--- GameDemo.java
+++ GameDemo.java
@@ -2,13 +2,27 @@
 
 import java.util.Scanner;
 import java.util.ArrayList;
-
+import java.util.Random;
 
 public class GameDemo {
 	public static void main(String[]args) {
+		Scanner kb = new Scanner(System.in);
 		ArrayList<Integer> vote = new ArrayList<Integer>();
 		ArrayList<Character> players = new ArrayList<Character>(); 
+		Random rn = new Random();
 		
+		System.out.print("Please enter your name: ");
+		String name = kb.nextLine();
+		
+		int role = rn.nextInt(8);
+		
+		new Character(name,role);
+		
+		
+		
+		
+
+
 		
 	}
 }
