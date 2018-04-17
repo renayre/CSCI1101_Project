868997d3e03d6924054174233b842cb7516d6424
sashap15 <37847974+sashap15@users.noreply.github.com>
2018-04-17 20:33:58 -0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -4,15 +4,16 @@
 public class Character {
 	private String name;
 	private boolean alive;
-	private Role role;
+	private String role;
 	private int votesAgainst;
 	private boolean safe;
-	private boolean isMafia; 
+	private boolean isMafia;
+	private String password;
 	
 	//DEFAULT CONSTRUCTOR THAT CREATES EVERY CHARACTER AS ALIVE
 	public Character () {}
 	//COMPLETE CONSTRUCTOR
-	public Character (String n, Role r) {
+	public Character (String n, String r, String p) {
 		name = n;
 		alive = true;
 		role = r;
@@ -27,13 +28,24 @@ public class Character {
 	public int getVotesAgainst() {return votesAgainst;}
 	public boolean getSafe() {return safe;}
 	public boolean getIsMafia() {return isMafia;}
+	public String getPassword() {return password;}
 	//SET METHODS
 	public void setName(String n) {name = n;}
 	public void setAlive(boolean a) {alive = a;}
-	public void setRole(Role r) {role = r;}
+	public void setRole(Integer r) {//int to be found by random number generator in demo class
+			if(r==1||r==2||r==3) 
+				role = "Mafia";
+			else if(r==4)
+				role = "Doctor";
+			else if(r==5)
+				role = "Police";
+			else
+				role = "Civilian";
+		}
 	public void setVotesAgainst(int v) {votesAgainst = v;}
 	public void setSafe(boolean s) {safe = s;}
 	public void setIsMafia(boolean m) {isMafia = m;}
+	public void setPassword(String s) {password = s;}
 	
 	public String toString() {
 		return "Name: " + name + "\nAlive: " + alive + "\nRole: " + role;
